7ff105cfa909d02729c561d79b483d5adc8bb3bd
Error323 <folkerthuizinga@gmail.com>
2020-04-30 22:07:26 +0200

    train fast

--- training/generator.py
+++ training/generator.py
@@ -1,35 +1,161 @@
 import glob
 import random
+import tensorflow as tf
+import numpy as np
+import struct
 
+NUM_PLANES = 49
+IMG_SIZE = 5
+NUM_MOVES = 180
+STATE = "30s5sB10s10s4s4sBBBBb720sb"
 
-class Generator:
-    def __init__(self, inputdir, shuffle, size):
+class Generator(tf.keras.utils.Sequence):
+    def __init__(self, inputdir, shuffle, batchsize):
+        self.record_struct = struct.Struct(STATE)
         self.shuffle = shuffle
+        self.batch_size = batchsize
         self.records = []
-        self.current = 0
-        filenames = glob.glob(inputdir + "/*.bin")
+
+        # load all games into memory (~56K per game) and store them per record
+        # for optimal shuffling.
+        filenames = glob.glob(str(inputdir) + "/*.bin")
+        recordsize = self.record_struct.size
         for name in filenames:
             with open(name, "rb") as f:
                 data = f.read()
             n = len(data)
-            assert n % size == 0
-            for i in range(0, n, size):
-                self.records.append(data[i:i+size])
+            assert n % recordsize == 0
+            for i in range(0, n, recordsize):
+                self.records.append(data[i:i+recordsize])
         print(f"Loaded {len(self.records)} records shuffle:{shuffle}")
 
 
     def __iter__(self):
         if self.shuffle:
             random.shuffle(self.records)
-        self.current = 0
         return self
 
 
-    def __next__(self):
-        if self.current >= len(self.records):
-            raise StopIteration
-        data = self.records[self.current]
-        self.current += 1
-        return data
+    def __len__(self):
+        return len(self.records) // self.batch_size
 
 
+    def __getitem__(self, index):
+        x, y = [], ([], [])
+        for i in range(self.batch_size):
+            s, p, z = self._create_planes(self.records[index * self.batch_size + i])
+            x.append(s)
+            y[0].append(p)
+            y[1].append(z)
+        return np.transpose(np.array(x), [0, 2, 3, 1]), (np.array(y[0]), np.array(y[1]))
+
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            random.shuffle(self.records)
+
+
+    def _create_planes(self, data):
+        """
+        1 + 1 + 5 + 5*4 + 15 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 49
+        |   |   |   |     |    |   |   |   |   |   |   |
+        |   |   |   |     |    |   |   |   |   |   |   them floor: v in {0,...,7}
+        |   |   |   |     |    |   |   |   |   |   them wall: v in {0, 1}
+        |   |   |   |     |    |   |   |   |   them left: v in {0,...,5}
+        |   |   |   |     |    |   |   |   us floor: v in {0,...,7}
+        |   |   |   |     |    |   |   us wall: v in {0, 1}
+        |   |   |   |     |    |   us left: v in {0,...,5}
+        |   |   |   |     |    1st tile: v in {-1, 0, 1}
+        |   |   |   |     center: v in {0,...,5}
+        |   |   |   factories: v in {0,...,5}
+        |   |   bag: v in {0,...,20}
+        |   them score: v in {0,...,255}
+        us score: v in {0,...,255}
+
+        All planes are normalized between [0, 1]
+        """
+        c, b, t, l1, l2, w1, w2, f1, f2, s1, s2, f, probs, winner = self.record_struct.unpack(data)
+
+        # make sure that the player who's turn it is, is always player 1. This
+        # ensures we create the planes from the current player's perspective.
+        if t == 1:
+            l1, l2 = l2, l1
+            w1, w2 = w2, w1
+            f1, f2 = f2, f1
+            s1, s2 = s2, s1
+
+        # create buffer
+        planes = np.zeros((NUM_PLANES, IMG_SIZE, IMG_SIZE), dtype=np.float32)
+
+        # scores
+        index = 0
+        planes[index, :, :] = s1 / 255
+        index += 1
+        planes[index, :, :] = s2 / 255
+        index += 1
+
+        # bag
+        for i in range(5):
+            planes[index, :, :] = b[i] / 20
+            index += 1
+
+        # factories
+        for i in range(5):
+            tiles = struct.unpack("BBBBB", c[i*5:i*5+5])
+            empty = 4
+            for tile, n in enumerate(tiles):
+                for _ in range(n):
+                    planes[index, :, :] = (tile + 1) / 5
+                    empty -= 1
+                    index += 1
+            for _ in range(empty):
+                index += 1
+
+        # center
+        tiles = struct.unpack("BBBBB", c[25:])
+        empty = 15
+        for tile, n in enumerate(tiles):
+            for _ in range(n):
+                planes[index, :, :] = (tile + 1) / 5
+                empty -= 1
+                index += 1
+        for _ in range(empty):
+            index += 1
+
+        # first tile
+        planes[index, :, :] = (f + 1) / 2
+        index += 1
+
+        # p1 left
+        for i in range(5):
+            tile, n = struct.unpack("BB", l1[i*2:i*2+2])
+            for j in range(n):
+                planes[index, i, j] = (tile + 1) / 5
+        index += 1
+
+        # p1 wall
+        planes[index, :, :] = np.unpackbits(np.frombuffer(w1, dtype=np.uint8))[7:].reshape(IMG_SIZE, IMG_SIZE).astype(np.float32)
+        index += 1
+
+        # p1 floor
+        planes[index, :, :] = f1 / 7
+        index += 1
+
+        # p2 left
+        for i in range(5):
+            tile, n = struct.unpack("BB", l2[i*2:i*2+2])
+            for j in range(n):
+                planes[index, i, j] = (tile + 1) / 5
+        index += 1
+
+        # p2 wall
+        planes[index, :, :] = np.unpackbits(np.frombuffer(w2, dtype=np.uint8))[7:].reshape(IMG_SIZE, IMG_SIZE).astype(np.float32)
+        index += 1
+
+        # p2 floor
+        planes[index, :, :] = f2 / 7
+        index += 1
+
+        assert index == NUM_PLANES
+
+        return planes, np.frombuffer(probs, dtype=np.float32), np.float32(winner)
--- training/train.py
+++ training/train.py
@@ -18,123 +18,8 @@ BATCH_SIZE = 512
 NUM_PLANES = 49
 IMG_SIZE = 5
 NUM_MOVES = 180
-STATE = "30s5sB10s10s4s4sBBBBb720sb"
-STATESIZE = struct.calcsize(STATE)
-SHUFFLE_BUFFER_SIZE = 100000
 
 reg = tf.keras.regularizers.l2(l=1e-4)
-
-# 1d planes for easy indexing
-flat_planes = []
-for i in range(256):
-    flat_planes.append(np.zeros(25, dtype=np.float32) + i)
-    
-
-def create_planes(data):
-    """
-    1 + 1 + 5 + 5*4 + 15 + 1 + 1 + 1 + 1 + 1 + 1 + 1 = 49
-    |   |   |   |     |    |   |   |   |   |   |   |
-    |   |   |   |     |    |   |   |   |   |   |   them floor: v in {0,...,7}
-    |   |   |   |     |    |   |   |   |   |   them wall: v in {0, 1}
-    |   |   |   |     |    |   |   |   |   them left: v in {0,...,5}
-    |   |   |   |     |    |   |   |   us floor: v in {0,...,7}
-    |   |   |   |     |    |   |   us wall: v in {0, 1}
-    |   |   |   |     |    |   us left: v in {0,...,5}
-    |   |   |   |     |    1st tile: v in {-1, 0, 1}
-    |   |   |   |     center: v in {0,...,5}
-    |   |   |   factories: v in {0,...,5}
-    |   |   bag: v in {0,...,20}
-    |   them score: v in {0,...,255}
-    us score: v in {0,...,255}
-    """
-    buf = struct.unpack(STATE, data.numpy())
-    c, b, t, l1, l2, w1, w2, f1, f2, s1, s2, f, probs, winner = buf
-
-    # scores
-    planes = flat_planes[s1].tobytes() + flat_planes[s2].tobytes()
-
-    # bag
-    for i in range(5):
-        planes += flat_planes[b[i]].tobytes()
-
-    # factories
-    for i in range(5):
-        tiles = struct.unpack("BBBBB", c[i*5:i*5+5])
-        empty = 4
-        for t, n in enumerate(tiles):
-            for _ in range(n):
-                planes += flat_planes[t + 1].tobytes()
-                empty -= 1
-        for _ in range(empty):
-            planes += flat_planes[0].tobytes()
-
-    # center
-    tiles = struct.unpack("BBBBB", c[25:])
-    empty = 15
-    for t, n in enumerate(tiles):
-        for _ in range(n):
-            planes += flat_planes[t + 1].tobytes()
-            empty -= 1
-    for _ in range(empty):
-        planes += flat_planes[0].tobytes()
-
-    # first tile
-    planes += (np.zeros(25, dtype=np.float32) + f).tobytes()
-
-    # p1 left
-    plane = np.zeros((5, 5), dtype=np.float32)
-    for i in range(5):
-        t, n = struct.unpack("BB", l1[i*2:i*2+2])
-        for j in range(n):
-            plane[i, j] = t + 1
-    planes += plane.tobytes()
-
-    # p1 wall
-    planes += np.unpackbits(np.frombuffer(w1, dtype=np.uint8))[7:].astype(np.float32).tobytes()
-
-    # p1 floor
-    planes += flat_planes[f1].tobytes()
-
-    # p2 left
-    plane = np.zeros((5, 5), dtype=np.float32)
-    for i in range(5):
-        t, n = struct.unpack("BB", l2[i*2:i*2+2])
-        for j in range(n):
-            plane[i, j] = t + 1
-    planes += plane.tobytes()
-
-    # p2 wall
-    planes += np.unpackbits(np.frombuffer(w2, dtype=np.uint8))[7:].astype(np.float32).tobytes()
-
-    # p2 floor
-    planes += flat_planes[f2].tobytes()
-
-    assert len(planes) == IMG_SIZE * IMG_SIZE * NUM_PLANES * 4
-
-    winner = float(winner)
-    winner = struct.pack('f', winner)
-
-    return planes, probs, winner
-    
-
-def parse_function(data):
-    """converts raw bytes containing (s, pi, z) to tensors"""
-    [planes, probs, winner] = tf.py_function(func=create_planes, inp=[data], Tout=(tf.string, tf.string, tf.string) )
-    s = tf.io.decode_raw(planes, tf.float32)
-    pi = tf.io.decode_raw(probs, tf.float32)
-    z = tf.io.decode_raw(winner, tf.float32)
-    s = tf.reshape(s, (IMG_SIZE, IMG_SIZE, NUM_PLANES))
-    pi = tf.reshape(pi, (NUM_MOVES,))
-    z = tf.reshape(z, (1,))
-    return s, (pi, z)
-
-
-def create_dataset(inputdir, shuffle):
-    gen = Generator(str(inputdir), shuffle, STATESIZE)
-    ds = tf.data.Dataset.from_generator(lambda: (x for x in gen), tf.string)
-    ds = ds.map(parse_function, num_parallel_calls=8)
-    ds = ds.batch(BATCH_SIZE)
-    return ds
 
 
 def conv(x, filters, kernel_size, strides=(1, 1)):
@@ -176,7 +61,7 @@ def resnet(x, blocks, filters):
 
 
 class LogLr(tf.keras.callbacks.Callback):
-    def __init__(self, steps=500):
+    def __init__(self, steps=100):
         self.steps = steps
         self.counter = 0
 
@@ -192,46 +77,42 @@ class LogLr(tf.keras.callbacks.Callback):
 
 def main(args):
     dst = args.output
-    wgt = dst / "weights"
     logdir = str(dst / "logs/scalars")
-    checkpoint_path = str(wgt)
+    model_path = dst / f"model-{args.blocks}x{args.filters}"
     dst.mkdir(parents=True, exist_ok=True)
 
 
-    train_ds = create_dataset(args.input / "train", True)
-    test_ds = create_dataset(args.input / "test", False)
-
-    """
-    for x, y in train_ds.take(1):
-        print(x)
-        print(y[0])
-        print(y[1])
-    """
+    train_ds = Generator(args.input / "train", True, BATCH_SIZE)
+    test_ds = Generator(args.input / "test", False, BATCH_SIZE)
 
     file_writer = tf.summary.create_file_writer(logdir + "/metrics")
     file_writer.set_as_default()
-    # Create a callback that saves the model's weights
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, verbose=1)
     # Log various metrics on tensorboard with this callback
-    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=logdir, update_freq=1000, profile_batch=0)
+    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=logdir, update_freq=100, profile_batch=0)
 
-    inputs = Input(shape=(IMG_SIZE, IMG_SIZE, NUM_PLANES), name="planes")
-    policy, value = resnet(inputs, args.blocks, args.filters)
-    model = Model(inputs, outputs=[policy, value], name=f"ResNet-{args.blocks}x{args.filters}")
-    losses = {"policy": "categorical_crossentropy", "value": "mean_squared_error"}
+    if model_path.exists():
+        print(f"Loading model from {model_path}")
+        model = tf.keras.models.load_model(str(model_path))
+    else:
+        inputs = Input(shape=(IMG_SIZE, IMG_SIZE, NUM_PLANES), name="planes")
+        policy, value = resnet(inputs, args.blocks, args.filters)
+        model = Model(inputs, outputs=[policy, value], name=f"ResNet-{args.blocks}x{args.filters}")
+        losses = {"policy": "categorical_crossentropy", "value": "mean_squared_error"}
 
-    print(model.summary())
+        print(model.summary())
 
-    # Compile and train
-    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=args.lr, momentum=0.9, nesterov=True),
-            loss=losses,
-            metrics=["accuracy"])
+        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=args.lr, momentum=0.9, nesterov=True),
+                loss=losses,
+                metrics=["accuracy"])
 
-    model.fit(train_ds, 
+    model.fit(x=train_ds, 
             steps_per_epoch=args.steps,
             validation_data=test_ds, 
-            validation_steps=60000//BATCH_SIZE, 
-            callbacks=[tensorboard_cb, checkpoint_cb, LogLr()])
+            callbacks=[tensorboard_cb, LogLr()],
+            workers=4,
+            use_multiprocessing=True)
+
+    model.save(str(model_path))
 
     return 0
 
@@ -240,8 +121,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AlphaZero Azul Trainer")
     parser.add_argument("--input", type=Path, \
             help="input directory containing test/ and train/ subdirs")
-    parser.add_argument("--resume", type=Path, default=None, \
-            help="resume from this model")
     parser.add_argument("--lr", type=float, \
             help="learningrate to use this session")
     parser.add_argument("--steps", type=int, default=1000, \
